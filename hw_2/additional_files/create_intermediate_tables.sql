cdaeae7b7f7e792589ed2ab4a077809b3f0d53e6
Maksim <dekstor235@gmail.com>
2024-11-07 18:12:58 +0300

    Add serial id in tables

--- hw_2/additional_files/create_intermediate_tables.sql
+++ hw_2/additional_files/create_intermediate_tables.sql
@@ -1,11 +1,13 @@
 CREATE TABLE student_courses (
+    id SERIAL PRIMARY KEY,
     student_id INTEGER REFERENCES students(id),
     course_id INTEGER REFERENCES courses(id),
-    PRIMARY KEY (student_id, course_id)
+    UNIQUE (student_id, course_id)
 );
 
 CREATE TABLE group_courses (
+    id SERIAL PRIMARY KEY,
     group_id INTEGER REFERENCES groups(id),
     course_id INTEGER REFERENCES courses(id),
-    PRIMARY KEY (group_id, course_id)
+    UNIQUE (group_id, course_id)
 );
